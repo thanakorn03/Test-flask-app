424998f0d4bb25e30634653b0519be5ace0258cc
thanakorn03 <iamok302@gmail.com>
2026-03-25 13:41:04 +0700

    Update hello message to hello from Thanakorn

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
-    return jsonify({"message": "Hello from Thanankorn"})
+    return jsonify({"message": "hello from Thanakorn"})
 
 @app.route('/api/goodbye', methods=['GET'])
 def goodbye():
